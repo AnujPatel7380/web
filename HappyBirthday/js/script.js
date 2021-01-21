6fd562c8369b50f487da68d5eaedf5f448524c73
Anuj Patel <50637377+AnujPatel7380@users.noreply.github.com>
2021-01-21 09:38:46 +0530

    a

---
+++ HappyBirthday/js/script.js
@@ -0,0 +1,25 @@
+var i = 0;
+function myFunction() {
+    i++;
+    switch (i) {
+        case 1:
+            document.getElementById("hello").innerHTML=i;
+            break;
+        case 2:
+            document.getElementById("hello").innerHTML = i;
+            break;
+        case 3:
+            document.getElementById("hello").innerHTML = i;
+            break;
+        case 4:
+            document.getElementById("hello").innerHTML = i;
+            break;
+        case 5:
+            document.getElementById("hello").innerHTML = i;
+            break;
+
+        default:
+            document.getElementById("hello").innerHTML = i;
+
+    }
+}
